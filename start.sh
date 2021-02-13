6888ee36aad084a4d0709dc472a35251f9758cc4
gigiali <ali.je@husky.neu.edu>
2021-02-13 00:00:43 -0500

    changed path in start.sh

--- start.sh
+++ start.sh
@@ -15,11 +15,13 @@ export PORT=4790
 
 echo "Stopping old copy of app, if any..."
 
-_build/prod/rel/practice/bin/practice stop || true
+#_build/prod/rel/practice/bin/practice stop || true
+_build/dev/rel/practice/bin/practice || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice start
+# _build/prod/rel/practice/bin/practice start
+_build/dev/rel/practice/bin/practice start
 
 # TODO: Add a systemd service file
 #       to start your app on system boot.
