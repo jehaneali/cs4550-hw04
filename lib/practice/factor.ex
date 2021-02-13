db9d849af754f1c1b8034ccf5a365f8e674c3e7e
gigiali <ali.je@husky.neu.edu>
2021-02-13 03:19:47 -0500

    working on factor

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -3,20 +3,33 @@ defmodule Practice.Factor do
   def factor(x) do
     #num = String.to_integer(x) <> "hello"
     #x <> "hey"
-    x = String.to_integer(x)
-    p = 2
-    #IO.puts(x<>"=")
-    cond do
-      #question: how do i do a loop?? very confused
-      x >= p * p ->
-        if rem(x, p) == 0 do
-          #IO.puts(p <> "*")
-          x = x / p
-        else
-          p = p + 1
-        end
-      true ->
-    end
+    p = String.to_integer(x)
+    divTwo(p)
+    #p = Integer.parse(x)
+    #p = 2
 
+    #IO.puts(x<>"=")
+    # cond do if x >= p * p
+    #   #question: how do i do a loop?? very confused
+    #     if rem(x, p) == 0 do
+    #       #IO.puts(p <> "*")
+    #       x = x / p
+    #     else
+    #       p = p + 1
+    #     end
+
+    # end
+
+  end
+
+  #trying again with alg from https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
+
+  def divTwo(x) do
+    cond do
+      rem(x,2) == 0 ->
+        divTwo(div(x,2))
+      true ->
+        x
+    end
   end
 end
